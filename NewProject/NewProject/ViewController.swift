f7245c1d18687fbea087f012f3138ad325c80304
Shohzod <shohzodrajabovmir@gmail.com>
2022-07-22 20:30:05 +0500

    nimadir_commit

---
+++ NewProject/NewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Shohzod Rajabov on 22/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
